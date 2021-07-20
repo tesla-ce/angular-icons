f0b806bf700f3d3a67dd7ccf47d3e265452c9f2b
Roger <roger@rcod.net>
2021-07-20 13:02:06 +0200

    ng test auto configurations

--- projects/tesla-ce/icons/karma.conf.js
+++ projects/tesla-ce/icons/karma.conf.js
@@ -25,8 +25,9 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    failOnEmptyTestSuite: false
   });
 };
